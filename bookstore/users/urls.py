aa48747558379b503b09801091174e31114b8091
xrs15001176394 <719371675@qq.com>
2018-04-09 10:54:33 +0800

    书城项目

---
+++ bookstore/users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from users import views
+
+urlpatterns = [
+    url(r'^register/$', views.register, name='register'), # 用户注册
+url(r'^register_handle/$', views.register_handle, name='register_handle'),
+]
